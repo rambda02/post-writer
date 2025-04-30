4daeb6cd9111b74868127d971f6a980e747a066b
lambda878 <lambda878@gmail.com>
2025-04-30 11:36:40 +0900

    ✨ ヘッダー、タイトルセクションを追加する

---
+++ src/app/(marketing)/layout.tsx
@@ -0,0 +1,35 @@
+import { MainNav } from "@/components/main-nav";
+import { buttonVariants } from "@/components/ui/button";
+import { marketingConfig } from "@/config/marketing";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+
+export const MarketingLayout = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <div>
+      <header className="container mx-auto px-8 z-40 bg-background">
+        <div className="h-20 py-6 flex items-center justify-between">
+          <MainNav items={marketingConfig.mainNav} />
+          <nav>
+            <Link
+              href="/login"
+              className={cn(
+                buttonVariants({ variant: "secondary", size: "sm" }),
+                "px-4 text-xs"
+              )}
+            >
+              ログイン
+            </Link>
+          </nav>
+        </div>
+      </header>
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default MarketingLayout;
---
+++ src/config/site.ts
@@ -0,0 +1,12 @@
+import { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+  name: "Post Writer",
+  description: "プログ投稿ができるサイトです。",
+  url: "http://localhost:3000",
+  ogImage: "",
+  links: {
+    x: "https://x.com",
+    github: "https://github.com/lambda878",
+  },
+};
