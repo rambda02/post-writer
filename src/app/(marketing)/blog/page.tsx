32676096a2731e9a75455b1a0ae75086bc84114f
rambda555 <rambda555@gmail.com>
2025-05-14 15:23:49 +0900

    🐛 本番環境ビルド時のエラーを解消する

--- src/app/(marketing)/blog/page.tsx
+++ src/app/(marketing)/blog/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { allPosts } from "contentlayer/generated";
-import { compareDesc, format } from "date-fns";
+import { compareDesc } from "date-fns";
 
 export const metadata = {
   title: "Blog",
