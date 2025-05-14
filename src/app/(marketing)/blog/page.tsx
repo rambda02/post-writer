cc39d61763ba5ada599e78c4b1f7e808ce55085a
rambda555 <rambda555@gmail.com>
2025-05-14 15:21:11 +0900

    ♻️ ブログページをリファクタリングする

--- src/app/(marketing)/blog/page.tsx
+++ src/app/(marketing)/blog/page.tsx
@@ -1,50 +1,75 @@
 import Link from "next/link";
 import Image from "next/image";
 import { allPosts } from "contentlayer/generated";
-import { format } from "date-fns";
+import { compareDesc, format } from "date-fns";
+
+export const metadata = {
+  title: "Blog",
+};
+
+function formatDate(date: string) {
+  return new Date(date).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+}
 
 export default function BlogPage() {
-  const posts = allPosts;
+  const posts = allPosts
+    .filter((post) => post.published)
+    .sort((a, b) => {
+      return compareDesc(new Date(a.date), new Date(b.date));
+    });
 
   return (
-    <div className="container mx-auto px-8 max-w-4xl py-6 lg:py-10">
-      <div>
-        <div className="space-y-4">
-          <h1 className="font-extrabold text-4xl lg:text-5xl tracking-tighter">
-            Blog 🚀
+    <div className="w-full mx-auto max-w-4xl px-8 py-6 lg:py-10">
+      <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
+        <div className="flex-1 space-y-4">
+          <h1 className="inline-block font-bold text-4xl tracking-tight lg:text-5xl">
+            Blog
           </h1>
-          <p className="text-muted-foreground text-xl">
-            ContentlayerとMDXで書いています。
+          <p className="text-xl text-muted-foreground">
+            A blog built using Contentlayer. Posts are written in MDX.
           </p>
         </div>
       </div>
       <hr className="my-8" />
-      <div className="grid sm:grid-cols-2 gap-10">
-        {posts.map((post, index) => (
-          <article key={post._id} className="relative flex flex-col space-y-2">
-            {post.image && (
-              <Image
-                src={post.image}
-                alt={post.title}
-                width={804}
-                height={452}
-                className="rounded-md border bg-muted"
-                priority={index === 0}
-              />
-            )}
-            <h2 className="text-2xl font-extrabold">{post.title}</h2>
-            {post.description && (
-              <p className="text-muted-foreground">{post.description}</p>
-            )}
-            {post.date && (
-              <p className="text-sm text-muted-foreground">
-                {format(post.date, "yyyy/MM/dd")}
-              </p>
-            )}
-            <Link href={post.slug} className="absolute inset-0"></Link>
-          </article>
-        ))}
-      </div>
+      {posts?.length ? (
+        <div className="grid gap-10 sm:grid-cols-2">
+          {posts.map((post, index) => (
+            <article
+              key={post._id}
+              className="group relative flex flex-col space-y-2"
+            >
+              {post.image && (
+                <Image
+                  src={post.image}
+                  alt={post.title}
+                  width={804}
+                  height={452}
+                  className="rounded-md border bg-muted transition-colors"
+                  priority={index <= 1}
+                />
+              )}
+              <h2 className="text-2xl font-extrabold">{post.title}</h2>
+              {post.description && (
+                <p className="text-muted-foreground">{post.description}</p>
+              )}
+              {post.date && (
+                <p className="text-sm text-muted-foreground">
+                  {formatDate(post.date)}
+                </p>
+              )}
+              <Link href={post.slug} className="absolute inset-0">
+                <span className="sr-only">View Article</span>
+              </Link>
+            </article>
+          ))}
+        </div>
+      ) : (
+        <p>No posts published.</p>
+      )}
     </div>
   );
 }
