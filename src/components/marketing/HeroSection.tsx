b6a7aa628aa902c7cef2a32e2367fc929b91a4d5
rambda555 <rambda555@gmail.com>
2025-05-20 15:36:00 +0900

    ♻️ Header セクションをリファクタリングする

--- src/components/marketing/HeroSection.tsx
+++ src/components/marketing/HeroSection.tsx
@@ -6,7 +6,7 @@ import { siteConfig } from "@/config/site"; // サイト設定
 export const HeroSection = () => {
   return (
     <section className="space-y-6 pb-8 pt-6 md:pb-12 md:pt-10 lg:py-32">
-      <div className="w-full mx-auto flex max-w-[64rem] flex-col items-center gap-4 text-center">
+      <div className="container flex max-w-[64rem] flex-col items-center gap-4 text-center">
         {/* ツイッターリンク */}
         <Link
           href={siteConfig.links.x}
@@ -17,13 +17,13 @@ export const HeroSection = () => {
         </Link>
 
         {/* 見出し */}
-        <h1 className="font-heading text-3xl sm:text-4xl md:text-5xl lg:text-7xl">
-          An example app built using Next.js 13 server components.
+        <h1 className="font-heading text-3xl sm:text-5xl md:text-6xl lg:text-7xl">
+          An example app built using Next.js 15 server components.
         </h1>
 
         {/* 説明 */}
         <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
-          I&apos;m building a web app with Next.js 13 and open sourcing
+          I&apos;m building a web app with Next.js 15 and open sourcing
           everything. Follow along as we figure this out together.
         </p>
 
