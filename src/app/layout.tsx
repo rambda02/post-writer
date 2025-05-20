4e6bd9c01f34c0c79f682d3d8592ad49e8ba4bdd
rambda555 <rambda555@gmail.com>
2025-05-20 10:32:40 +0900

    ♻️  cssファイルの修正、メタデータのリファクタリングをする

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata, Viewport } from "next"; // next.js のコアパッケージ
 import "./globals.css"; // グローバルスタイルシート
-import "./tailwind.css"; // テールウィンドウスタイルシート (tailwind.config.mjs の設定の移管先)
+// import "./tailwind.css"; // テールウィンドウスタイルシート (tailwind.config.mjs の設定の移管先)
 import { Inter as FontSans } from "next/font/google"; // グローバルフォント
 import localFont from "next/font/local"; // ローカルフォント
 import { siteConfig } from "@/config/site"; // サイト設定
@@ -22,53 +22,64 @@ const fontHeading = localFont({
   variable: "--font-heading",
 });
 
+// ビューポートの設定
 export const viewport: Viewport = {
+  // テーマカラーの設定　（デフォルトはライトモード）
+  // モバイルブラウザのステータスバーの色、デスクトップブラウザのアドレスバーの色を設定
   themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
+    { media: "(prefers-color-scheme: light)", color: "white" }, // ライトモードのテーマカラー
+    { media: "(prefers-color-scheme: dark)", color: "black" }, // ダークモードのテーマカラー
   ],
 };
 
+// メタデータの設定
 export const metadata: Metadata = {
+  // タイトルの設定
   title: {
-    default: siteConfig.name,
-    template: `%s | ${siteConfig.name}`,
+    default: siteConfig.name, // デフォルトのタイトル
+    template: `%s | ${siteConfig.name}`, // テンプレートのタイトル
   },
+  // 説明の設定
   description: siteConfig.description,
+  // キーワードの設定 (SEO対策、検索エンジンのためのキーワード)
   keywords: [
     "Next.js",
     "React",
     "Tailwind CSS",
-    "shadcn/ui",
-    "NextAuth",
-    "Prisma",
+    "Server Components",
+    "Radix UI",
   ],
+  // 著者の設定 (コンテンツの著者として、より詳細な情報（URLを含む）を提供)
   authors: [
     {
       name: "rambda",
       url: "https://rambda.com",
     },
   ],
-  creator: "rambda",
+  // 作成者の設定 （シンプルに作成者名のみを指定）
+  creator: "@rambda",
+  // Open Graphの設定 (SEO対策、SNSシェアのための設定)
   openGraph: {
-    type: "website",
-    locale: "ja",
-    url: siteConfig.url,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
+    type: "website", // ウェブサイトの設定
+    locale: "ja", // 言語の設定
+    url: siteConfig.url, // サイトのURL
+    title: siteConfig.name, // タイトル
+    description: siteConfig.description, // 説明
+    siteName: siteConfig.name, // サイトの名前
   },
+  // Twitterの設定 (SEO対策、SNSシェアのための設定)
   twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    images: [`${siteConfig.url}/og.png`],
-    creator: "@rambda",
+    card: "summary_large_image", // カードの設定
+    title: siteConfig.name, // タイトル
+    description: siteConfig.description, // 説明
+    images: [`${siteConfig.url}/og.png`], // 画像
+    creator: "@rambda", // 作成者
   },
+  // アイコンの設定
   icons: {
-    icon: "/favicon.ico",
-    // shortcut: "/favicon-16x16.png",
-    // apple: "/apple-touch-icon.png",
+    icon: "/favicon.ico", // アイコンの設定
+    // shortcut: "/favicon-16x16.png", // ショートカットアイコンの設定
+    // apple: "/apple-touch-icon.png", // iOSデバイスのアイコンの設定
   },
   manifest: `${siteConfig.url}/site.webmanifest`, // ウェブアプリケーションの設定を定義
 };
@@ -83,21 +94,21 @@ export default function RootLayout({
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased", // フォントが読み込まれるまではfont-sansが適用される
-          fontSans.variable,
-          fontHeading.variable
+          fontSans.variable, // フォントの設定
+          fontHeading.variable // ヘッダーのフォントの設定
         )}
       >
         <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
+          attribute="class" // テーマの属性 (class: クラスを使用してテーマを適用)
+          defaultTheme="system" // デフォルトのテーマ
+          enableSystem // システムテーマの有効化
+          disableTransitionOnChange // トランジションの無効化
         >
           {children}
-          <Analytics />
-          <Toaster />
-          <TailwindIndicator />
-        </ThemeProvider>
+          <Analytics /> {/* アナリティクス */}
+          <Toaster /> {/* トースター */}
+          <TailwindIndicator /> {/* インジケーター */}
+        </ThemeProvider> {/* テーマプロバイダー */}
       </body>
     </html>
   );
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,13 +1,18 @@
 import { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
-  name: "Taxonomy",
+  // サイトの名前
+  name: "Post Writer",
+  // サイトの説明
   description:
     "An open source application built using the new router, server components and everything new in Next.js 15.",
+  // サイトのURL
   url: process.env.NEXTAUTH_URL || "http://localhost:3000",
+  // サイトのOGP画像
   ogImage: `${process.env.NEXTAUTH_URL}/og.png`,
+  // リンクの設定
   links: {
-    x: "https://x.com/rambda555",
-    github: "https://github.com/rambda555/post-writer",
+    x: "https://x.com/rambda555", // Xのリンク
+    github: "https://github.com/rambda555/post-writer", // GitHubのリンク
   },
 };
