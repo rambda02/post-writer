28088657552bddfc6565fb5f84709fa70a405fb0
rambda555 <rambda555@gmail.com>
2025-05-20 16:54:22 +0900

    🎨 Features セクションのカードをコンポーネント化する

--- src/components/marketing/FeaturesSection.tsx
+++ src/components/marketing/FeaturesSection.tsx
@@ -1,87 +1,44 @@
-import { Icon } from "@/components/icon"; // アイコン
+import { featuresCardData } from "@/data/features"; // Features データ
 
 export const FeaturesSection = () => {
   return (
     <section
       id="features"
-      className="w-full max-w-[1400px] mx-auto space-y-6 bg-slate-50 py-8 dark:bg-transparent md:py-12 lg:py-24"
+      className="container space-y-6 bg-slate-50 py-8 dark:bg-transparent md:py-12 lg:py-24"
     >
+      {/* 見出し */}
       <div className="mx-auto flex max-w-[58rem] flex-col items-center space-y-4 text-center">
-        <h2 className="font-bold text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
+        <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
           Features
         </h2>
         <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
           This project is an experiment to see how a modern app, with features
           like auth, subscriptions, API routes, and static pages would work in
-          Next.js 13 app dir.
+          Next.js 15 app dir.
         </p>
       </div>
+
+      {/* Features カード */}
       <div className="mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-3">
-        <div className="relative overflow-hidden rounded-lg border bg-background p-2">
-          <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
-            <Icon.nextjs />
-            <div className="space-y-2">
-              <h3 className="font-bold">Next.js 13</h3>
-              <p className="text-sm text-muted-foreground">
-                App dir, Routing, Layouts, Loading UI and API routes.
-              </p>
+        {featuresCardData.map((featuresCard, index) => (
+          <div
+            key={index}
+            className="relative overflow-hidden rounded-lg border bg-background p-2"
+          >
+            <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
+              <featuresCard.icon />
+              <div className="space-y-2">
+                <h3 className="font-bold">{featuresCard.title}</h3>
+                <p className="text-sm text-muted-foreground">
+                  {featuresCard.description}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="relative overflow-hidden rounded-lg border bg-background p-2">
-          <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
-            <Icon.react />
-            <div className="space-y-2">
-              <h3 className="font-bold">React 18</h3>
-              <p className="text-sm">Server and Client Components. Use hook.</p>
-            </div>
-          </div>
-        </div>
-        <div className="relative overflow-hidden rounded-lg border bg-background p-2">
-          <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
-            <Icon.planetscale />
-            <div className="space-y-2">
-              <h3 className="font-bold">Database</h3>
-              <p className="text-sm text-muted-foreground">
-                ORM using Prisma and deployed on PlanetScale.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="relative overflow-hidden rounded-lg border bg-background p-2">
-          <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
-            <Icon.tailwind />
-            <div className="space-y-2">
-              <h3 className="font-bold">Components</h3>
-              <p className="text-sm text-muted-foreground">
-                UI components built using Radix UI and styled with Tailwind CSS.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="relative overflow-hidden rounded-lg border bg-background p-2">
-          <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
-            <Icon.auth />
-            <div className="space-y-2">
-              <h3 className="font-bold">Authentication</h3>
-              <p className="text-sm text-muted-foreground">
-                Authentication using NextAuth.js and middlewares.
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="relative overflow-hidden rounded-lg border bg-background p-2">
-          <div className="flex h-[180px] flex-col justify-between rounded-md p-6">
-            <Icon.stripe />
-            <div className="space-y-2">
-              <h3 className="font-bold">Subscriptions</h3>
-              <p className="text-sm text-muted-foreground">
-                Free and paid subscriptions using Stripe.
-              </p>
-            </div>
-          </div>
-        </div>
+        ))}
       </div>
+
+      {/* 説明 */}
       <div className="mx-auto text-center md:max-w-[58rem]">
         <p className="leading-normal text-muted-foreground sm:text-lg sm:leading-7">
           Taxonomy also includes a blog and a full-featured documentation site
