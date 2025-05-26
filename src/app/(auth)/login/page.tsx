5af45f9029966a6b12159e665c6ca7a29fc4f436
rambda555 <rambda555@gmail.com>
2025-05-26 12:22:25 +0900

    💄 ログインページのデザインを更新する

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,24 +1,45 @@
-import Link from "next/link";
-import { UserAuthForm } from "@/components/user-auth-form";
+import { Metadata } from "next"; // Next.js のコアパッケージ
+import Link from "next/link"; // Next.js のリンクコンポーネント (リンクを表示する)
+import { cn } from "@/lib/utils"; // ユーティリティ関数ライブラリ
+import { buttonVariants } from "@/components/ui/button"; // ボタンコンポーネント (ボタンの表示を行う)
+import { UserAuthForm } from "@/components/user-auth-form"; // ユーザー認証フォームコンポーネント (ユーザー認証フォームを表示する)
+import { Icon } from "@/components/Icon"; // アイコンコンポーネント (アイコンを表示する)
+
+export const metadata: Metadata = {
+  title: "Login",
+  description: "Login to your account",
+};
 
 export default function Login() {
   return (
-    <div className="container mx-auto px-8 flex flex-col justify-center h-screen items-center w-screen">
+    <div className="mx-auto px-8 flex flex-col items-center justify-center h-screen w-screen">
+      <Link
+        href="/"
+        className={cn(
+          buttonVariants({ variant: "ghost", size: "lg" }),
+          "absolute left-4 top-4 md:left-8 md:top-8"
+        )}
+      >
+        <Icon.chevronLeft />
+        Back
+      </Link>
       <div className="mx-auto w-full sm:w-[350px] flex flex-col justify-center space-y-6">
-        <div className="text-center space-y-2">
+        <div className="flex flex-col text-center space-y-2">
+          <Icon.logo className="mx-auto h-6 w-6" />
           <h1 className="text-2xl font-semibold tracking-tight">
             Welcome Back
           </h1>
           <p className="text-sm text-muted-foreground">
-            メールアドレスを入力してログインできます。
+            Enter your email to sign in to your account
           </p>
         </div>
 
+        {/* ユーザー認証フォーム */}
         <UserAuthForm />
 
-        <p className="text-muted-foreground px-8 text-center text-sm">
+        <p className="px-8 text-center text-sm text-muted-foreground">
           <Link href="/register" className="underline underline-offset-4">
-            アカウントを持っていませんか？
+            Don&apos;t have an account? Sign Up
           </Link>
         </p>
       </div>
