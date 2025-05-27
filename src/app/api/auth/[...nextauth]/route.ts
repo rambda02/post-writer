95c03fa7326cb421ddf0368ea2d9180d39bcb49a
rambda555 <rambda555@gmail.com>
2025-05-27 10:21:22 +0900

    ✨ Email ログイン機能を追加する

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,9 @@
-import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth";
+import NextAuth from "next-auth"; // NextAuth.js のコアパッケージ
 
+import { authOptions } from "@/lib/auth"; // NextAuth.js の設定
+
+// NextAuth.js のハンドラーを作成する
 const handler = NextAuth(authOptions);
 
+// GET と POST リクエストをハンドルする
 export { handler as GET, handler as POST };
--- src/components/user-auth-form.tsx
+++ src/components/user-auth-form.tsx
@@ -1,24 +1,78 @@
 "use client";
 
 import { useState } from "react"; // React　のコアパッケージ
-import { signIn } from "next-auth/react"; // NextAuth の認証機能を実装するためのパッケージ　　(OAuth認証、データベース統合、JWTトークン、セッション管理などの認証機能を提供)
+import { useSearchParams } from "next/navigation"; // クエリパラメータを取得するためのパッケージ
+import { signIn } from "next-auth/react"; // NextAuth の認証機能を実装するためのパッケージ　　(OAuth認証、データベース統合、JWTトークン、セッション管理などの認証機能を提供する)
+import { userAuthSchema } from "@/lib/validations/auth"; // バリデーションスキーマ (バリデーションを行うためのスキーマ)
+import { useForm } from "react-hook-form"; // フォーム管理を簡単にするためのライブラリ (フォームの状態管理、バリデーション、エラーハンドリングを効率的に行うための一連のフックを提供する)
+import { z } from "zod"; // バリデーションを行うためのパッケージ
+import { zodResolver } from "@hookform/resolvers/zod"; // バリデーションを行うためのライブラリ (バリデーションを行うためのスキーマを解決する)
 import { cn } from "@/lib/utils"; // ユーティリティ関数ライブラリ
 import { Input } from "./ui/input"; // 入力コンポーネント (入力フォームを表示する)
 import { buttonVariants } from "./ui/button"; // ボタンコンポーネント (ボタンの表示を行う)
 import { Icon } from "@/components/Icon"; // アイコンコンポーネント (アイコンを表示する)
 import { Label } from "@/components/ui/label"; // ラベルコンポーネント (ラベルを表示する)
+import { toast } from "sonner"; // トーストメッセージを表示するためのパッケージ (通知を表示する)
+
+// フォームのデータ型を定義 (バリデーションを行う)
+type FormData = z.infer<typeof userAuthSchema>;
 
 export const UserAuthForm = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => {
-  // const [isLoading, setIsLoading] = useState<boolean>(false)
-  const [isGitHubLoading, setIsGitHubLoading] = useState<boolean>(false);
-  const [isGoogleLoading, setIsGoogleLoading] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false); // ローディング状態を管理する
+  const [isGitHubLoading, setIsGitHubLoading] = useState<boolean>(false); // GitHub ローディング状態を管理する
+  const [isGoogleLoading, setIsGoogleLoading] = useState<boolean>(false); // Google ローディング状態を管理する
+  const searchParams = useSearchParams(); // クエリパラメータを取得する
+
+  // フォームのデータを管理する (主にバリデーションを行う)
+  const {
+    register, // フォーム要素を登録する関数
+    handleSubmit, // フォーム送信を処理する関数
+    formState: { errors }, // バリデーションエラー情報
+  } = useForm<FormData>({
+    // バリデーションを行う
+    resolver: zodResolver(userAuthSchema),
+  });
+
+  /**
+   * フォームのデータを送信する
+   * @param data (FormData)
+   * @returns トーストメッセージを表示する
+   */
+  async function onSubmit(data: FormData) {
+    // ローディング状態を有効にする
+    setIsLoading(true);
+
+    // メールアドレスを送信する
+    const signInResult = await signIn("email", {
+      email: data.email.toLowerCase(), // メールアドレスを小文字に変換する
+      redirect: false, // リダイレクトを無効にする
+      callbackUrl: searchParams?.get("from") || "/dashboard", // リダイレクト先のURLを設定する
+    });
+
+    // ローディング状態を無効にする
+    setIsLoading(false);
+
+    // サインインに失敗した場合
+    if (!signInResult?.ok) {
+      return toast.error("Something went wrong.", {
+        description: "Your sign in request failed. Please try again.",
+        duration: 5000, // 5秒間表示（ミリ秒単位）
+      });
+    }
+
+    // サインインに成功した場合
+    return toast.success("Check your email", {
+      description: "We sent you a login link. Be sure to check your spam too.",
+      duration: 5000, // 5秒間表示（ミリ秒単位）
+    });
+  }
 
   return (
     <div className={cn("grid gap-6", className)} {...props}>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="grid gap-2">
           <div className="grid gap-1">
             <Label className="sr-only" htmlFor="email">
@@ -32,11 +86,21 @@ export const UserAuthForm = ({
               autoComplete="email"
               autoCorrect="off"
               className="h-10"
-              // disabled={isLoading || isGitHubLoading}
-              // {...register("email")}
+              disabled={isLoading || isGitHubLoading || isGoogleLoading}
+              {...register("email")}
             />
+            {/* バリデーションエラーがある場合はエラーメッセージを表示する */}
+            {errors?.email && (
+              <p className="px-1 text-xs text-destructive">
+                {errors.email.message}
+              </p>
+            )}
           </div>
-          <button className={cn(buttonVariants({ size: "lg" }))}>
+          <button
+            className={cn(buttonVariants({ size: "lg" }))}
+            disabled={isLoading}
+          >
+            {isLoading && <Icon.spinner />}
             Sign In with Email
           </button>
         </div>
