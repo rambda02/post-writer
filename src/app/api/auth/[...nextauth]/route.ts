fb12d80cdf51509d5579718af5dfcb3550ac6905
rambda555 <rambda555@gmail.com>
2025-05-28 18:00:40 +0900

    💡 コメントを追加・編集する

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -5,5 +5,5 @@ import { authOptions } from "@/lib/auth"; // NextAuth.js の設定
 // NextAuth.js のハンドラーを作成する
 const handler = NextAuth(authOptions);
 
-// GET と POST リクエストをハンドルする
+// GET と POST リクエストに対するハンドラーを指定する
 export { handler as GET, handler as POST };
--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,22 +2,30 @@ import { PrismaClient } from "@prisma/client";
 
 declare global {
   // eslint-disable-next-line no-var
-  var cachePrisma: PrismaClient;
+  var cachePrisma: PrismaClient; // Prisma クライアント型のグローバル変数を宣言 (サーバーにキャッシュするため)
 }
 
-let prisma: PrismaClient;
+// Prisma クライアントを宣言
+let prisma: PrismaClient; // Prisma クライアント型の変数を宣言
 
-// 本番環境の場合
+// 本番環境の場合は新しい Prisma クライアントを作成　 
+// 開発環境ではホットリロードで不要なインスタンスが作成されてしまうため、
+// サーバーキャッシュを使用することでメモリの使用を最適化を行う
 if (process.env.NODE_ENV === "production") {
+  // Prisma クライアントを作成
   prisma = new PrismaClient();
 
   // それ以外の環境の場合
 } else {
   // サーバーキャッシュがない場合
   if (!global.cachePrisma) {
+    // サーバーキャッシュを作成
     global.cachePrisma = new PrismaClient();
   }
+
+  // サーバーキャッシュがある場合はサーバーキャッシュを使用
   prisma = global.cachePrisma;
 }
 
+// Prisma クライアントをエクスポートする
 export const db = prisma;
