5a456fe05b4fae4dd5f1285999ee6f373f80651a
rambda555 <rambda555@gmail.com>
2025-06-21 11:31:54 +0900

    💡 コメントを追加する

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,20 +1,24 @@
-import { NextAuthOptions } from "next-auth";
-import Github from "next-auth/providers/github";
-import Google from "next-auth/providers/google";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { db } from "./db";
+import { NextAuthOptions } from "next-auth"; // NextAuth.js のコアパッケージ
+import Github from "next-auth/providers/github"; // GitHub を使用した認証を提供するパッケージ
+import Google from "next-auth/providers/google"; // Google を使用した認証を提供するパッケージ
+import { PrismaAdapter } from "@auth/prisma-adapter"; // Prisma のアダプター (Prisma を NextAuth.js と連携させるためのパッケージ)
+import { db } from "./db"; // Prisma クライアント (Prisma を使用してデータベースに接続)
 
 export const authOptions: NextAuthOptions = {
+  // 認証プロバイダーの設定 (認証方法の設定)
   providers: [
+    // GitHub 認証プロバイダーの設定
     Github({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-      allowDangerousEmailAccountLinking: true,
+      allowDangerousEmailAccountLinking: true, // 同じメールアドレスを持つ異なる認証プロバイダーのログインを許可する設定
     }),
+
+    // Google 認証プロバイダーの設定
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      allowDangerousEmailAccountLinking: true,
+      allowDangerousEmailAccountLinking: true, // 同じメールアドレスを持つ異なる認証プロバイダーのログインを許可する設定
     }),
   ],
   adapter: PrismaAdapter(db),
