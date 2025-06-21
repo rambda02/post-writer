71e10051cbbb30255f41b28e580c8a9806d2d078
rambda555 <rambda555@gmail.com>
2025-06-22 01:59:30 +0900

    💡 コメントを追加する

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,26 +4,39 @@ import { NextResponse } from "next/server";
 
 export default withAuth(
   async function middleware(req) {
+    // jwt トークンを取得する　（ブラウザの cookie から）
     const token = await getToken({ req });
+
+    // トークンの有無でログイン状態を判定する
     const isAuth = !!token;
+
+    // 認証ページ　（ログイン/登録ページ）　かどうかを判定する
     const isAuthPage =
       req.nextUrl.pathname.startsWith("/login") ||
       req.nextUrl.pathname.startsWith("/register");
 
+    // 認証ページの場合
     if (isAuthPage) {
+      // ログインしている場合
       if (isAuth) {
+        // ダッシュボードページにリダイレクトする
         return NextResponse.redirect(new URL("/dashboard", req.url));
       }
+
+      // ログインしていない場合は認証ページを表示する
       return null;
     }
 
+    // ログインしていない場合
     if (!isAuth) {
+      // ログインページにリダイレクトする
       return NextResponse.redirect(new URL("/login", req.url));
     }
   },
   {
     callbacks: {
       async authorized() {
+        // withAuth の内部チェックをバイパス （独自のチェックロジックを使用するため）
         return true;
       },
     },
@@ -31,5 +44,6 @@ export default withAuth(
 );
 
 export const config = {
+  // ミドルウェアを適応するパスを指定する
   matcher: ["/dashboard/:path*", "/editor/:path*", "/login", "/register"],
 };
