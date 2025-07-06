80430714203dd71866058c2cc27db9d690235d2a
rambda555 <rambda555@gmail.com>
2025-07-06 21:21:44 +0900

    💄 サインアップ画面を更新する

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,44 +1,57 @@
+import { Metadata } from "next";
 import Link from "next/link";
-import { UserAuthForm } from "@/components/user-auth-form";
-import { buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import { UserAuthForm } from "@/components/user-auth-form";
+import { Icons } from "@/components/Icons";
+
+export const metadata: Metadata = {
+  title: "Create an account",
+  description: "Create an account to get started.",
+};
 
 export default function Register() {
   return (
-    <div className="container mx-auto px-8 grid flex-col lg:grid-cols-2 h-screen w-screen items-center justify-center lg:max-w-none lg:px-0">
+    <div className="container grid flex-col lg:grid-cols-2 h-screen w-screen items-center justify-center lg:max-w-none lg:px-0">
       <Link
         href={"/login"}
         className={cn(
           buttonVariants({ variant: "ghost" }),
-          "absolute left-4 md:left-8 md:top-8"
+          "absolute right-4 top-4 md:right-8 md:top-8"
         )}
       >
-        ログイン
+        Login
       </Link>
       <div className="h-full bg-muted lg:block hidden" />
-      <div className="mx-auto w-full sm:w-[350px] flex flex-col justify-center space-y-6">
-        <div className="text-center space-y-2">
-          <h1 className="text-2xl font-semibold tracking-tight">
-            アカウントの作成
-          </h1>
-          <p className="text-sm text-muted-foreground">
-            メールアドレスを入力してアカウントを作成してください。
+      <div className="lg:p-8">
+        <div className="mx-auto w-full sm:w-[350px] flex flex-col justify-center space-y-6">
+          {/* ヘッダー */}
+          <div className="flex flex-col text-center space-y-2">
+            <Icons.logo className="mx-auto h-6 w-6" />
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Create an account
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Enter your email below to create your account
+            </p>
+          </div>
+
+          {/* ユーザー認証フォーム */}
+          <UserAuthForm />
+
+          {/* 利用規約とプライバシーポリシー */}
+          <p className="text-muted-foreground px-8 text-center text-sm">
+            By clicking continue, you agree to our{" "}
+            <Link href="/terms" className="underline underline-offset-4">
+              Terms of Service
+            </Link>{" "}
+            and{" "}
+            <Link href="/privacy" className="underline underline-offset-4">
+              Privacy Policy
+            </Link>
+            .
           </p>
         </div>
-
-        <UserAuthForm />
-
-        <p className="text-muted-foreground px-8 text-center text-sm">
-          続けてクリックすれば私たちの
-          <Link href="/terms" className="underline underline-offset-4">
-            利用規約
-          </Link>
-          と
-          <Link href="/privacy" className="underline underline-offset-4">
-            プライバシーポリシー
-          </Link>
-          に同意したことになります。
-        </p>
       </div>
     </div>
   );
