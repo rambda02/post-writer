26007c62ee7a6afd2f97208ccdaba67cfb7ba711
rambda02 <rambda02@gmail.com>
2025-08-12 05:06:08 +0900

    ♻️ ダッシュボードページのメインコンテンツのヘッダーをリファクタリングする

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -1,21 +1,21 @@
 interface DashboardHeaderProps {
-  heading: string;
-  text?: string;
-  children?: React.ReactNode;
+  heading: string
+  text?: string
+  children?: React.ReactNode
 }
 
-export const DashboardHeader = ({
+export function DashboardHeader({
   heading,
   text,
   children,
-}: DashboardHeaderProps) => {
+}: DashboardHeaderProps) {
   return (
     <div className="flex items-center justify-between px-2">
       <div className="grid gap-1">
-        <h1 className="font-extrabold text-3xl md:text-4xl">{heading}</h1>
+        <h1 className="font-heading text-3xl md:text-4xl">{heading}</h1>
         {text && <p className="text-lg text-muted-foreground">{text}</p>}
       </div>
       {children}
     </div>
-  );
-};
+  )
+}
